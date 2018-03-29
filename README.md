6266262fa2db07a22f52cf2fdc6e90e6ca8b02c9
Zhao <peng-feiz@hpe.com>
2018-03-29 11:10:35 +0800

    basic operations on singleLinkedList

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ It's the thinking that matters, not the recollection and senseless coding.
   - Make things more complicated when combination these basic algorithms to solve advanced problem.
   - Concern to Advanced Math and Probability Theory approaches
 
-## what and why asymptotic analysis
+## asymptotic analysis
 Although there're many factors about the execution time ( and space usage ) of an algorithm, for two given algorithms with the same input size, if they run on the same machine, the most principal factor will be the input size. Here're something to pay attention to:
  * When we talk about that one algorithms is faster than another, we ignored other factors except input size and the input size is large enough. In short, we need to judge which one is the high-order infinity of another, the L'Opida Rule will be quite useful here.
  * Since inputs may vary a lot ( for example, the order of the original array play a as decisive as the algorithm  role in the execution time ),  we can not directly give the result of asymptotic analysis for an explicit input case. Therefore when we talk the complexity of an algorithm, we usually indicate the worst complexity. 
@@ -21,8 +21,22 @@ Shortcomings asymptotic analysis
 ### Big O notation
 https://en.wikipedia.org/wiki/Big_O_notation#Family_of_Bachmann.E2.80.93Landau_notations
 
-## sorting
+## Algorithms
+### sorting
 | Plugin | Key Point | Complexity Analysis | Best  | Worst  | Average |
 | ------ | ------ | ------ | ------ | ------ | ------ |
 | quick sort | Quick sort is a up-to-down solution, it's the partition what matters most in this algo, we need use an integer to track number of elements which is bigger than the pivot and place the new found element to the proper position | T(n) = T(k) + T(n-k) + a*n | happens when the pivot always partition the array into two balance part, that is, T(n) = 2(T/2) + a*n = n*T(1) + a*n*㏒₂n| happens when the pivot is always the largest element in an increase order sort.  k=1, T(n) = nT(1) + a*(n+2)*(n-1)/2, T(n)=O(n²) | 
 | merge sort | an easy sort algo with no critical operation | Always nLog₂n | nLog₂n | nLog₂n | nLog₂n |
+
+## DataStructure
+### singleLinkedList
+1. singleLinkedList vs array:
+	+ advantage
+		+ LL has more efficient insertion and deletion(O(1)); 
+		+ dynamic size.
+	+ disadvantage
+		+ worse efficienet access (O(n));
+		+ without cache locality;
+		+ single node has a large space because of the pointer, but since LL size is dynamic, which will get one more bonus.
+	+ storage area
+		+ generally, array is allocated memory on Data section (global array) or Stack section (local array), sinlgeLinkedList is allocated dynamically (during run-time).
--- singleLinkedList.cpp
+++ singleLinkedList.cpp
@@ -30,6 +30,94 @@ void reverseList(node** singleLinkedList){
     }
     (*singleLinkedList)-> next = q;
 }
+void insertHead(node** listHeadRef, int new_data)
+{
+	if((*listHeadRef) != NULL || (*listHeadRef)->value >= 0){
+	    ((*listHeadRef)->value) ++;
+	    node* newNode = new node;
+	    newNode->value = new_data;
+	    newNode->next = (*listHeadRef)->next;
+	    (*listHeadRef)->next = newNode;
+	}
+}
+
+void insertAfter(node* head, node* prev_node, int new_data)
+{
+    (head->value)++;
+	node* newNode = new node;
+	newNode->value = new_data;
+	newNode->next = prev_node->next;
+	prev_node->next = newNode;
+}
+
+void append(node** head_ref, int new_data)
+{
+    (*head_ref)->value ++;
+	node* last = *head_ref;
+	while(last->next != NULL){
+	    last = last->next;
+	}
+	last->next = new node;
+	last->next->next = NULL;
+	last->next->value = new_data;
+}
+
+void remove(node* singleLinkedList, int key, bool onlyFirst){
+    node* temp = singleLinkedList->next;
+    node* prev = singleLinkedList;
+    node* del = NULL;
+    while(temp != NULL){
+        if(temp->value != key){
+            prev = temp;
+            temp = temp->next;
+        } else {
+            del = temp;
+            temp = del->next;
+            prev->next = del->next;
+            delete del;
+            (singleLinkedList->value)--;
+            if(onlyFirst){
+                break;
+            }
+        }
+    }
+}
+
+void removeAt(node* singleLinkedList, int position){
+    node* del = singleLinkedList->next;
+    node* prev = singleLinkedList;
+    int index = 1;
+    while(index < position && del != NULL){
+        index++;
+        prev = del;
+        del = del->next;
+    }
+    if(del != NULL){
+        prev->next = del->next;
+        delete del;
+        singleLinkedList->value --;
+    }
+}
+//iteractive
+void correctLength(node* singleLinkedList){
+    node* p = singleLinkedList->next;
+    int length = 0;
+    while(p!=NULL){
+        length++;
+        p = p->next;
+    }
+    singleLinkedList->value = length;
+    cout << "length: " << length << endl;
+}
+//recursive
+int  getLength(node* head){
+   if(head != NULL ) {
+       return 1+getLength(head->next);
+   } else {
+       return 0;
+   }
+}
+
 int main() {
 	node* singleLinkedList = new node;
 	node* p = singleLinkedList;
@@ -42,5 +130,15 @@ int main() {
 	printSingleLinkedList(singleLinkedList);
 	reverseList(&singleLinkedList);
 	printSingleLinkedList(singleLinkedList);
+	append(&singleLinkedList, 6);
+	printSingleLinkedList(singleLinkedList);
+    insertHead(&singleLinkedList, 7);
+    printSingleLinkedList(singleLinkedList);
+    insertHead(&singleLinkedList, 1);
+    printSingleLinkedList(singleLinkedList);
+    append(&singleLinkedList, 4);
+    printSingleLinkedList(singleLinkedList);
+    insertAfter(singleLinkedList, singleLinkedList->next, 8);
+    printSingleLinkedList(singleLinkedList);
 	return 0;
 }
